a27712c4afb13321f302403bf765f2acedcaa775
shinnosuke <shinnosuke1028@foxmail.com>
2019-12-20 23:40:49 +0800

    Reload_2

--- ftp_monitor/src/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding:utf-8 -*-
-# @Author: Shin
-# @Date: 2019/12/5 11:10
-# @File: __init__.py.py
--- ftp_monitor/src/func_demo/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding:utf-8 -*-
-# @Author: Shin
-# @Date: 2019/12/1 21:29
-# @File: __init__.py.py
